9022c192d95a9c761a6a4e25119067df5b3f2d55
Canito301 <franfuentealba2016@gmail.com>
2025-07-22 19:40:05 -0400

    Commit de modificación de javadoc luego de actualizacion de clase enfrentamiento, GestorEstadoEncuentro, GestorNotificaciones y SimuladorPuntaje .

--- Proyecto_java/ProyectodeJava/src/main/java/org/Logica/GestorNotificaciones.java
+++ Proyecto_java/ProyectodeJava/src/main/java/org/Logica/GestorNotificaciones.java
@@ -1,12 +1,35 @@
 package org.Logica;
 
+/**
+ * La clase {@code GestorNotificaciones} se encarga de gestionar las notificaciones
+ * relacionadas con los eventos del torneo. Actúa como intermediario para notificar
+ * a los observadores registrados en el {@link SujetoTorneo}.
+ * <p>
+ * Actualmente, esta clase notifica cuando los resultados de un {@link Enfrentamiento}
+ * han sido actualizados.
+ * </p>
+ *
+ * @author Canito301
+ */
 public class GestorNotificaciones {
+
+    /** Referencia al sujeto del torneo que gestiona los observadores. */
     private final SujetoTorneo torneo;
 
+    /**
+     * Crea una instancia de {@code GestorNotificaciones} con el torneo especificado.
+     *
+     * @param torneo el objeto {@link SujetoTorneo} que gestionará las notificaciones a los observadores.
+     */
     public GestorNotificaciones(SujetoTorneo torneo) {
         this.torneo = torneo;
     }
 
+    /**
+     * Notifica a los observadores que los resultados de un enfrentamiento han sido actualizados.
+     *
+     * @param e el {@link Enfrentamiento} cuyos resultados han cambiado.
+     */
     public void notificarResultadosActualizados(Enfrentamiento e) {
         torneo.notificarObservadores(TipoEvento.RESULTADOS_ACTUALIZADOS, e);
     }
